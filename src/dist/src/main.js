72bcb60b9bfa191b5b19c4706560e18438ad9f99
lollipop99 <supports@accounts.com>
2026-03-20 19:29:28 +0100

    update

--- src/dist/src/main.js
+++ src/dist/src/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.enableCors({
         origin: [
             "http://localhost:3000",
-            "https://cleaning-frontend.netlify.app",
+            "https://hosannaglobal.co.uk",
         ],
         credentials: true,
     });
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
  app.enableCors({
   origin: [
     "http://localhost:3000",
-    "https://cleaning-frontend.netlify.app",
+    "https://hosannaglobal.co.uk",
   ],
   credentials: true,
 });
